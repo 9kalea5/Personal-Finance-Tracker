6f13d50a0c62ccb14ba41a65efd5e14f66576897
kaleab <alebachewkaleab@gmail.com>
2025-05-31 20:29:51 +0300

    Authentication Complete

--- core/models.py
+++ core/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, PermissionsMixin
-import uuid
 from django.utils import timezone
+import uuid
+from .managers import CustomUserManager  # make sure this import is correct
 
 class User(AbstractUser, PermissionsMixin):
+    username = None  # <--- REMOVE username
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(max_length=255, unique=True)
     phone = models.CharField(max_length=20, unique=True)
@@ -11,9 +13,11 @@ class User(AbstractUser, PermissionsMixin):
     last_name = models.CharField(max_length=255, blank=True, default="")
     date_joined = models.DateTimeField(default=timezone.now)
     is_staff = models.BooleanField(default=False)
-    
-    REQUIRED_FIELDS = ['first_name', 'last_name', 'phone']
+
     USERNAME_FIELD = 'email'
-    
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'phone']
+
+    objects = CustomUserManager()  # <--- Use the custom manager
+
     def __str__(self):
         return self.email
--- core/views.py
+++ core/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins, status
+from rest_framework.response import Response
+from .models import User
+from .serializers import UserSerializer, UserCreateSerializer
 
-# Create your views here.
+class UserViewSet(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  viewsets.GenericViewSet):
+    queryset = User.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return UserCreateSerializer
+        return UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response(UserSerializer(user).data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
