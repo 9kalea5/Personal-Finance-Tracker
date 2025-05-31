47cf8b14b31366d388076fd1d5a9f04898330c26
kaleab <alebachewkaleab@gmail.com>
2025-05-31 21:23:37 +0300

    Froget Password

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from .views import UserViewSet
+from .views import UserViewSet, PasswordResetRequestView
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
     path('', include(router.urls)),
+     path('password-reset/', PasswordResetRequestView.as_view(), name='password_reset'),
 ]
--- core/views.py
+++ core/views.py
@@ -2,10 +2,16 @@ from rest_framework import viewsets, mixins, status
 from rest_framework.response import Response
 from .models import User
 from .serializers import UserSerializer, UserCreateSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import get_user_model
+from django.core.mail import send_mail
 
 class UserViewSet(mixins.ListModelMixin,
                   mixins.CreateModelMixin,
                   viewsets.GenericViewSet):
+    User = get_user_model()
     queryset = User.objects.all()
 
     def get_serializer_class(self):
@@ -19,3 +25,15 @@ class UserViewSet(mixins.ListModelMixin,
             user = serializer.save()
             return Response(UserSerializer(user).data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class PasswordResetRequestView(APIView):
+    def post(self, request):
+        email = request.data.get("email")
+        if not email:
+            return Response({"detail": "Email is required"}, status=400)
+
+        try:
+            user = User.objects.get(email=email)
+            return Response({"detail": "Password reset email sent."}, status=200)
+        except User.DoesNotExist:
+            return Response({"detail": "User with this email does not exist."}, status=404)
