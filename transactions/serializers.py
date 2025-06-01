2d182cb13f3891619b940ef33dd97c71737aaa63
kaleab <alebachewkaleab@gmail.com>
2025-06-01 13:23:23 +0300

    Wallet serializer

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -10,3 +10,22 @@ class WalletSerializer(serializers.ModelSerializer):
     class Meta:
         model = Wallet
         fields = '__all__'
+
+class TransactionSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+    category_id = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all(), source='category', write_only=True
+    )
+    wallet = WalletSerializer(read_only=True)
+    wallet_id = serializers.PrimaryKeyRelatedField(
+        queryset=Wallet.objects.all(), source='wallet', write_only=True, required=False
+    )
+
+    class Meta:
+        model = Transaction
+        fields = [
+            'id', 'user', 'title', 'amount', 'transaction_type',
+            'category', 'category_id', 'wallet', 'wallet_id',
+            'note', 'created_at', 'updated_at'
+        ]
+        read_only_fields = ['user', 'created_at', 'updated_at']
