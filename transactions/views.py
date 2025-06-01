0ca2da7309acbcd357fc276c1b514eb7a88fd63c
kaleab <alebachewkaleab@gmail.com>
2025-06-01 13:56:55 +0300

    Category View Set

--- transactions/views.py
+++ transactions/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.filters import SearchFilter, OrderingFilter
+from .models import Transaction, Category, Wallet, Budget, Goal
+from .serializers import TransactionSerializer, CategorySerializer, WalletSerializer, BudgetSerializer, GoalSerializer
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+class CategoryViewSet(ModelViewSet):
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['name', 'category_type']
+    ordering_fields = ['name', 'category_type']
+    
+    def get_queryset(self):
+        return Category.objects.filter(user=self.request.user)
+    
+    def destroy(self, request, *args, **kwargs):
+        category = self.get_object()
+        
+        if category.transaction_set.exists():
+            return Response(
+                {'error': 'Category cannot be deleted because it is used in transactions.'},
+                status=status.HTTP_405_METHOD_NOT_ALLOWED
+            )
+
+        return super().destroy(request, *args, **kwargs)
