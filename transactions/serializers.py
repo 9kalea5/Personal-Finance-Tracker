80480bd66cad4ac8e23c83dba153296d1385d6cb
kaleab <alebachewkaleab@gmail.com>
2025-06-01 13:22:29 +0300

    Wallet serializer

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -1,2 +1,12 @@
 from rest_framework import serializers
 from .models import Transaction, Category, Wallet, Goal, Budget
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+class WalletSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Wallet
+        fields = '__all__'
