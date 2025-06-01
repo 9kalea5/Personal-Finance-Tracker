060a7f5326b7a1d4da3bb9af69682c6e119e494e
kaleab <alebachewkaleab@gmail.com>
2025-06-01 13:27:49 +0300

    Category and Wallet admin page

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+from .models import Transaction, Category, Wallet, Goal, Budget
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+
+@admin.register(Wallet)
+class WalletAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'name', 'balance', 'created_at')
+    search_fields = ('name', 'user__email')
+    list_filter = ('created_at',)
